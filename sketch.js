58f1f6b206cbbdf65b22915c8ea72b761e55e357
KATHERINE YASBETH TENECELA MEJIA <katherine.tenecela@ucuenca.edu.ec>
2023-10-26 16:20:17 -0500

    sonido

--- sketch.js
+++ sketch.js
@@ -1,7 +1,17 @@
+let osc, playing, freq, amp;
+let t = 0;
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  osc = new p5.Oscillator("sawtooth");
 }
 
 function draw() {
-  circle(mouseX, mouseY, 20);
+  background(220);
+  osc.start();
+  freq = constrain(map(noise(t), 0, 1, 500, 900), 500, 900);
+  amp = 0.5;
+
+  osc.freq(freq, 0.1);
+  osc.amp(amp, 0.1);
+  t += 0.01;
 }
